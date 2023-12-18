294ed779d7b4152f221fa9bef86b508cddf39c60
quirua1702 <tranvanqui170202@gmail.com>
2023-12-18 16:48:00 +0700

    init project

---
+++ app/Http/Controllers/TinhTrangController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TinhTrang;
+use Illuminate\Http\Request;
+
+class TinhTrangController extends Controller
+{
+         public function getDanhSach()
+        {
+                $tinhtrang = TinhTrang::all();
+                return view('admin.tinhtrang.danhsach', compact('tinhtrang'));
+        }
+        public function getThem()
+        {
+                return view('admin.tinhtrang.them');
+        }
+        public function postThem(Request $request)
+        {
+                $request->validate([
+                        'tinhtrang' => ['required', 'max:255', 'unique:tinhtrang'],
+        ]);
+
+                $orm = new TinhTrang();
+                $orm->tinhtrang = $request->tinhtrang;
+                $orm->save();
+                return redirect()->route('admin.tinhtrang');
+        }
+        public function getSua($id)
+        {
+                $tinhtrang = TinhTrang::find($id);
+                return view('admin.tinhtrang.sua', compact('tinhtrang'));
+        }
+        public function postSua(Request $request, $id)
+        {
+                $request->validate([
+                        'tinhtrang' => ['required', 'max:255', 'unique:tinhtrang,tinhtrang,'.$id],
+        ]);             
+                $orm = TinhTrang::find($id);
+                $orm->tinhtrang = $request->tinhtrang;
+                $orm->save();
+
+                return redirect()->route('admin.tinhtrang');
+        }
+        public function getXoa($id)
+        {
+                $orm = TinhTrang::find($id);
+
+                $orm->delete();
+                return redirect()->route('admin.tinhtrang');
+        }
+}
